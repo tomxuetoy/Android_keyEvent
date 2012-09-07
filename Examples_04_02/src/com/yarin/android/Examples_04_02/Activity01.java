158279100d67da73b820772ead5762a7b03d0cac
Tom Xue <tomxue0126@gmail.com>
2012-09-08 00:17:16 +0800

    use a button to trigger the BACK keyEvent but still failed

--- Examples_04_02/src/com/yarin/android/Examples_04_02/Activity01.java
+++ Examples_04_02/src/com/yarin/android/Examples_04_02/Activity01.java
@@ -3,20 +3,32 @@ package com.yarin.android.Examples_04_02;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity01 extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
 	}
 
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		// 这里构建KeyEvent对象，其功能为返回键的功能
 		// 因此我们按任意键都会执行返回键功能
-		KeyEvent key = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK);
+		// KeyEvent key = new KeyEvent(KeyEvent.ACTION_DOWN,
+		// KeyEvent.KEYCODE_BACK);
+
+		Button button = (Button) findViewById(R.id.button1);
+		/* 监听button的事件信息; Tom Xue: Anonymous Inner Classes */
+		button.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,
+						KeyEvent.KEYCODE_BACK));
+				Activity01.this.finish();
+			}
+		});
 
 		// 这里传入的参数就是我们自己构建的KeyEvent对象key
-		return super.onKeyDown(key.getKeyCode(), key);
+		 return super.onKeyDown(keyCode, event);
 	}
 }
